8fbce83981c6a7256d1b6f1c592a912a4c3062ec
JawadAmmar700 <JawadAmmarCode700@gmail.com>
2023-04-20 14:36:09 +0300

    tt

--- app.js
+++ app.js
@@ -8,10 +8,11 @@ app.use(bodyParser.json());
 app.use(cors({ origin: "*", optionsSuccessStatus: 200 }));
 
 app.get("/", (req, res) => {
-  cron.schedule("*/3 * * * * *", () => {
-    console.log("I'm running every minute");
-  });
   res.send("Hey there!, this is a server that is used by the senior project");
+});
+
+cron.schedule("* * * * * *", () => {
+  console.log("I'm running every minute");
 });
 
 app.listen(4000);
