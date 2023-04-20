844e71e1b27b073fe6af1d32598d053b8e04c8f5
JawadAmmar700 <JawadAmmarCode700@gmail.com>
2023-04-20 17:19:34 +0300

    test13

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-require("./monitor");
+const cron = require("node-cron");
+
+// require("./monitor");
 
 app.use(bodyParser.json());
 app.use(cors({ origin: "*", optionsSuccessStatus: 200 }));
 
 app.get("/", (req, res) => {
   res.send("Hey there!, this is a server that is used by the senior project");
+});
+
+app.post("/", (req, res) => {
+  cron.schedule("*/3 * * * * *", () => {
+    console.log("I'm running every 3 s");
+  });
+  res.json({ message: "I'm running every 3 s" });
 });
 
 app.listen(4000);
